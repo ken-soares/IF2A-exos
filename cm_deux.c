27d706df466c1078348fd9c6f2663086b309a762
Kenneth SOARES <kensoares@gmail.com>
2023-09-21 11:17:15 +0200

    ajout deuxieme partie de l'exo

--- cm_deux.c
+++ cm_deux.c
@@ -5,11 +5,23 @@
 #include "decla_exos.h"
 void cm_deux_exo() {
     srand(time(NULL));
-    int n;
+    int n,a,b;
     printf("entrer un n:");
     scanf("%d", &n);
 
     for(int i = 0; i < n; i++){
         printf("i:%d val:%d\n",i, rand() % 49 + 1);
     }
+
+    printf("entrer un a:");
+    scanf("%d", &a);
+    printf("entrer un b:");
+    scanf("%d", &b);
+
+    if(a < 0 || b < 0){
+        printf("a: %d, b: %d,signe du produit negatif.\n",a,b);
+    }else{
+        printf("a: %d, b: %d,signe du produit positif.\n",a,b);
+    }
+
 }
