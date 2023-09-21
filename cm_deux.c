67e69503a8090e25fd3237e0dc802efbd0d3cd4c
Kenneth SOARES <kensoares@gmail.com>
2023-09-21 11:29:41 +0200

    updated exos

--- cm_deux.c
+++ cm_deux.c
@@ -3,16 +3,20 @@
 //
 
 #include "decla_exos.h"
-void cm_deux_exo() {
+
+void partie_un(){
     srand(time(NULL));
-    int n,a,b;
+    int n;
     printf("entrer un n:");
     scanf("%d", &n);
 
     for(int i = 0; i < n; i++){
         printf("i:%d val:%d\n",i, rand() % 49 + 1);
     }
+}
 
+void partie_deux(){
+    int a, b;
     printf("entrer un a:");
     scanf("%d", &a);
     printf("entrer un b:");
@@ -23,5 +27,30 @@ void cm_deux_exo() {
     }else{
         printf("a: %d, b: %d,signe du produit positif.\n",a,b);
     }
+}
 
+void partie_trois(){
+    int N, sum = 0, produit = 1;
+    printf("entrer un N:");
+    scanf("%d", &N);
+    for(int i = 1; i < N+1; i++){
+        int tmp  = 0;
+        printf("\nentrer un nombre");
+        scanf("%d", &tmp);
+        sum += tmp;
+        produit *= tmp;
+    }
+
+    printf("somme:%d, produit: %d, moyenne:%.4f", sum, produit, (float)sum/(float)N);
+}
+
+void cm_deux_exo() {
+
+    // partie 1
+    partie_un();
+    // partie 2
+    partie_deux();
+
+    // partie 3
+    partie_trois();
 }
--- main.c
+++ main.c
@@ -3,30 +3,30 @@
 
 int main() {
 
-    // exo 5.2
-    cinq_deux_exo();
-
-    // pour les exercices 6.1 et 6.2 voir "exos.md"
-
-    // exo 6.3
-    six_trois_exo();
-
-    // exo 6.4
-    six_quatre_exo();
-
-    // exo 6.5
-    six_cinq_exo();
-
-    // exo 6.6
-    six_six_exo();
-
-    //exo 6.7
-    six_sept_exo();
-
-    //exo 6.8
-    six_huit_exo();
-
-    //exo CM 2
+//   exo 5.2
+//   cinq_deux_exo();
+//
+//   pour les exercices 6.1 et 6.2 voir "exos.md"
+//
+//   exo 6.3
+//   six_trois_exo();
+//
+//   exo 6.4
+//   six_quatre_exo();
+//
+//   exo 6.5
+//   six_cinq_exo();
+//
+//   exo 6.6
+//   six_six_exo();
+//
+//   exo 6.7
+//   six_sept_exo();
+//
+//   exo 6.8
+//   six_huit_exo();
+//
+//   exo CM 2
     cm_deux_exo();
 
     return 0;
